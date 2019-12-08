16c400d5d752dfc98346d891c268a7f56c37d101
sachitnagpal <sachit.nagpal@unfound.news>
2019-12-08 20:12:34 +0530

    Code Improvements - Removed Unnecessary Screen Module, Added Configuration, etc.

---
+++ configuration.py
@@ -0,0 +1,25 @@
+import os
+
+# SETTINGS PANEL NAME (Displayed on sidebar of settings in app)
+PRESET_NAME = 'Configuration'
+
+# Settings Panel Defaults
+JSON_SETTINGS_PATH = "/Users/sachitnagpal/upenn/image_replays/settings.json"
+DEFAULT_IDX = 0
+DEFAULT_DF_PATH = os.getcwd()
+DEFAULT_FOR_KEY = "ProblemId,StepNumber"
+DEFAULT_PRIMARY_KEY = "problemid,stepnumber"
+DEFAULT_PIC_URL = "PicUrl"
+DEFAULT_QUES_TEXT = "clean_questiontext"
+DEFAULT_PROB_ID = "problemid"
+DEFAULT_STEP_NUM = "stepnumber"
+
+# KEYCODES
+KEYCODE_NEXT = 40  # 40 - Enter key pressed
+
+# TEXT
+
+INITIAL_MESSAGE = "Please set the configuration from settings"
+UNABLE_TO_LOAD_MESSAGE = "Please select the correct dataset path from settings"
+COMPLETED_MESSAGE = "Reached end of file"
+
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 import pandas as pd
 
 from kivy.app import App
@@ -13,46 +11,41 @@ from kivy.core.window import Window
 from kivy.uix.settings import SettingsWithSidebar
 from kivy.config import ConfigParser
 
-
-configuration = ConfigParser()
-configuration.read('screenmanager.ini')
+from configuration import *
 
 
-def merge_crosswalks(img_df, ques_df):
-    return ques_df.merge(img_df, left_on=["problemid", "stepnumber"], right_on=["ProblemId", "StepNumber"])
+def merge_crosswalks(img_df, ques_df, config):
+    primary_keys = config.get('replays', 'PRIMARY_KEY').split(',')
+    foreign_keys = config.get('replays', 'FOREIGN_KEY').split(',')
+    return ques_df.merge(img_df, left_on=primary_keys, right_on=foreign_keys)
 
 
-def get_ques_text(df, idx):
-    return df.iloc[idx]['clean_questiontext'] if idx < len(df) else "Reached end of file"
+def get_ques_text(df, idx, col):
+    return df.iloc[idx][col] if idx < len(df) else COMPLETED_MESSAGE
 
 
-def get_img_url(df, idx):
-    return df.iloc[idx]['PicUrl'] if idx < len(df) else None
+def get_img_url(df, idx, col):
+    return df.iloc[idx][col] if idx < len(df) else None
 
 
-def get_prob_id(df, idx):
-    return int(df.iloc[idx]['problemid'] if idx < len(df) else 0)
+def get_prob_id(df, idx, col):
+    return int(df.iloc[idx][col] if idx < len(df) else 0)
 
 
-def get_step_num(df, idx):
-    return int(df.iloc[idx]['stepnumber'] if idx < len(df) else 0)
+def get_step_num(df, idx, col):
+    return int(df.iloc[idx][col] if idx < len(df) else 0)
 
 
-def get_replay_data(df, idx):
-    ques_text = get_ques_text(df, idx)
-    img_url = get_img_url(df, idx)
-    prob_id = get_prob_id(df, idx)
-    step_num = get_step_num(df, idx)
+def get_replay_data(df, idx, config):
+    ques_text = get_ques_text(df, idx, config.get("replays", "QUESTION_TEXT"))
+    img_url = get_img_url(df, idx, config.get("replays", "PIC_URL"))
+    prob_id = get_prob_id(df, idx, config.get("replays", 'PROBLEM_ID'))
+    step_num = get_step_num(df, idx, config.get("replays", "STEP_NUM"))
 
     return prob_id, step_num, ques_text, img_url
 
 
-
-class ReplayScreen(Screen):
-    pass
-
-
-class MyScreenManager(ScreenManager):
+class ReplayScreen(BoxLayout):
     pass
 
 
@@ -60,18 +53,24 @@ class ScreenManagerApp(App):
 
     prob_id = NumericProperty(0)
     step_num = NumericProperty(0)
-    ques_text = StringProperty("Please select dataset path from settings!")
+    ques_text = StringProperty(INITIAL_MESSAGE)
     img_url = StringProperty(None)
     idx = NumericProperty(0)
 
     def build_config(self, config):
         config.setdefaults('replays', {
-            'idx': 0,
-            'img_df_path': os.getcwd(),
-            'ques_df_path': os.getcwd()})
+            'IDX': DEFAULT_IDX,
+            'IMG_DF_PATH': DEFAULT_DF_PATH,
+            'FOREIGN_KEY': DEFAULT_FOR_KEY,
+            'QUES_DF_PATH': DEFAULT_DF_PATH,
+            'PRIMARY_KEY': DEFAULT_PRIMARY_KEY,
+            'PIC_URL': DEFAULT_PIC_URL,
+            'QUESTION_TEXT': DEFAULT_QUES_TEXT,
+            'PROBLEM_ID': DEFAULT_PROB_ID,
+            'STEP_NUM': DEFAULT_STEP_NUM})
 
     def build_settings(self, settings):
-        settings.add_json_panel('Configuration', self.config, "settings.json")
+        settings.add_json_panel(PRESET_NAME, self.config, JSON_SETTINGS_PATH)
 
     def build(self):
         self.settings_cls = SettingsWithSidebar
@@ -79,52 +78,52 @@ class ScreenManagerApp(App):
 
         Window.bind(on_key_down=self._on_keyboard_down)
 
-        self.idx = int(self.config.get('replays', 'idx'))
+        self.idx = int(self.config.get('replays', 'IDX'))
 
         try:
-            img_df = pd.read_csv(self.config.get('replays', 'img_df_path'))
-            ques_df = pd.read_csv(self.config.get('replays', 'ques_df_path'))
-            self.df = merge_crosswalks(img_df, ques_df)
-            self.prob_id, self.step_num, self.ques_text, self.img_url = get_replay_data(self.df, self.idx)
-        except (FileNotFoundError, KeyError) as e:
-            self.ques_text = "Please select the correct dataset path from settings"
+            img_df = pd.read_csv(self.config.get('replays', 'IMG_DF_PATH'))
+            ques_df = pd.read_csv(self.config.get('replays', 'QUES_DF_PATH'))
+            self.df = merge_crosswalks(img_df, ques_df, self.config)
+            self.prob_id, self.step_num, self.ques_text, self.img_url = get_replay_data(self.df, self.idx, self.config)
+        except:
+            self.ques_text = UNABLE_TO_LOAD_MESSAGE
 
-        return self.root
+        return ReplayScreen()
 
     def _on_keyboard_down(self, instance, keyboard, keycode, text, modifiers):
-        if keycode == 40:  # 40 - Enter key pressed
+        if keycode == KEYCODE_NEXT:
             self.next_replay()
 
     def next_replay(self):
         try:
             self.idx += 1
-            self.prob_id, self.step_num, self.ques_text, self.img_url = get_replay_data(self.df, self.idx)
-            self.config.set('replays', 'idx', self.idx)
+            self.prob_id, self.step_num, self.ques_text, self.img_url = get_replay_data(self.df, self.idx, self.config)
+            self.config.set('replays', 'IDX', self.idx)
             self.config.write()
-        except AttributeError:
-            self.ques_text = "Please select dataset path from settings!"
+        except:
+            self.ques_text = UNABLE_TO_LOAD_MESSAGE
 
     def prev_replay(self):
         if self.idx != 0:
             try:
                 self.idx -= 1
-                self.prob_id, self.step_num, self.ques_text, self.img_url = get_replay_data(self.df, self.idx)
-                self.config.set('replays', 'idx', self.idx)
+                self.prob_id, self.step_num, self.ques_text, self.img_url = get_replay_data(self.df, self.idx, self.config)
+                self.config.set('replays', 'IDX', self.idx)
                 self.config.write()
-            except AttributeError:
-                self.ques_text = "Please select dataset path from settings!"
+            except:
+                self.ques_text = UNABLE_TO_LOAD_MESSAGE
 
     def on_config_change(self, config, section, key, value):
 
-        self.idx = int(self.config.get('replays', 'idx'))
+        self.idx = int(self.config.get('replays', 'IDX'))
 
         try:
-            img_df = pd.read_csv(config.get('replays', 'img_df_path'))
-            ques_df = pd.read_csv(config.get('replays', 'ques_df_path'))
-            self.df = merge_crosswalks(img_df, ques_df)
-            self.prob_id, self.step_num, self.ques_text, self.img_url = get_replay_data(self.df, self.idx)
-        except (FileNotFoundError, KeyError) as e:
-            self.ques_text = "Please select the correct dataset path from settings"
+            img_df = pd.read_csv(config.get('replays', 'IMG_DF_PATH'))
+            ques_df = pd.read_csv(config.get('replays', 'QUES_DF_PATH'))
+            self.df = merge_crosswalks(img_df, ques_df, self.config)
+            self.prob_id, self.step_num, self.ques_text, self.img_url = get_replay_data(self.df, self.idx, self.config)
+        except:
+            self.ques_text = UNABLE_TO_LOAD_MESSAGE
 
 
 if __name__ == "__main__":
